3bed28ab8f003e77abcbff207dd439b43550016e
hrmnns <hermanns@gmx.net>
2026-03-11 05:35:46 +0000

    Deploying to gh-pages from @ hrmnns/litebistudio@1580a32c748e21c1fed756f46e52b50e085158f9 🚀

--- assets/useBackupStatus-ytWRAo6w.js
+++
@@ -1,1 +0,0 @@
-import{b as u}from"./index-Ci8QTWMC.js";const c=()=>{const{lastBackup:a,changeCount:e,markBackupComplete:t}=u();return{lastBackup:a?new Date(a):null,changeCount:e,isBackupRecommended:e>0,markBackupComplete:t}};export{c as u};
